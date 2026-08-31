bc0c338b363c69e06c3822ba8c63c6423c54bd82
Raju <mhraju069@gmail.com>
2026-08-31 15:11:38 +0600

    feat: implement Storage class for reading and writing database pages to disk

--- mydb/storage.py
+++ mydb/storage.py
@@ -1,19 +1,51 @@
 from pathlib import Path
 
+from .page import PAGE_SIZE, Page
+
+
 class Storage:
     def __init__(self, path: str):
         self.path = Path(path)
 
-    def write(self, filename: str, data: bytes):
+        self.path.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
 
-        file_path = self.path / filename
-        
-        with open(file_path, "wb") as file:
-            file.write(data)
+        self.file = open(
+            self.path,
+            "a+b"
+        )
 
-    def read(self, filename: str) -> bytes :
-        
-        file_path = self.path / filename
-        
-        with open(file_path, "rb") as file:
-            return file.read()
+    def page_count(self) -> int:
+        self.file.seek(0, 2)
+
+        size = self.file.tell()
+
+        return size // PAGE_SIZE
+
+    def write_page(self, page_id: int, page: Page):
+        offset = page_id * PAGE_SIZE
+
+        self.file.seek(offset)
+
+        self.file.write(page.raw())
+
+        self.file.flush()
+
+    def read_page(self, page_id: int) -> Page:
+        offset = page_id * PAGE_SIZE
+
+        self.file.seek(offset)
+
+        data = self.file.read(PAGE_SIZE)
+
+        if len(data) != PAGE_SIZE:
+            raise ValueError(
+                f"Page {page_id} does not exist"
+            )
+
+        return Page(data)
+
+    def close(self):
+        self.file.close()
